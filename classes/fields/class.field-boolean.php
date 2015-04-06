211e8c74eaec0a7fe35a55c65debbe3b4387596c
Hiroshi Urabe <mail@torounit.com>
2015-04-06 15:26:07 +0900

    Boolean field

---
+++ classes/fields/class.field-boolean.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * Smart_Custom_Fields_Field_Boolean
+ * Version    : 1.0.0
+ * Author     : Toro_Unit, Takashi Kitajima
+ * Created    : April 6, 2015
+ * Modified   : April 6, 2015
+ * License    : GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+class Smart_Custom_Fields_Field_Boolean extends Smart_Custom_Fields_Field_Base {
+
+	/**
+	 * 必須項目の設定
+	 *
+	 * @return array
+	 */
+	protected function init() {
+		return array(
+			'type'         => 'boolean',
+			'display-name' => __( 'Boolean', 'smart-custom-fields' ),
+			'optgroup'     => 'basic-fields',
+		);
+	}
+
+	/**
+	 * 設定項目の設定
+	 *
+	 * @return array
+	 */
+	protected function options() {
+		return array(
+			'default'     => 0,
+			'notes'       => '',
+			'true_label'  => __( 'Yes', 'smart-custom-fields' ),
+			'false_label' => __( 'No', 'smart-custom-fields' ),
+		);
+	}
+
+	/**
+	 * 投稿画面にフィールドを表示
+	 *
+	 * @param int $index インデックス番号
+	 * @param mixed $value 保存されている値（check のときだけ配列）
+	 *
+	 * @return string html
+	 */
+	public function get_field( $index, $value ) {
+		$name     = $this->get_field_name_in_editor( $index );
+		$disabled = $this->get_disable_attribute( $index );
+
+		$true = sprintf(
+			'<label><input type="radio" name="%s" value="1" class="widefat" %s %s />%s</label>',
+			esc_attr( $name ),
+			checked( 1, $value, false ),
+			disabled( true, $disabled, false ),
+			$this->get( 'true_label' )
+		);
+
+		$false = sprintf(
+			'<label><input type="radio" name="%s" value="0" class="widefat" %s %s />%s</label>',
+			esc_attr( $name ),
+			checked( 0, $value, false ),
+			disabled( true, $disabled, false ),
+			$this->get( 'false_label' )
+		);
+
+		return $true . $false;
+	}
+
+	/**
+	 * 設定画面にフィールドを表示（オリジナル項目）
+	 *
+	 * @param int $group_key
+	 * @param int $field_key
+	 */
+	public function display_field_options( $group_key, $field_key ) {
+		?>
+		<tr>
+			<th><?php esc_html_e( 'Default', 'smart-custom-fields' ); ?></th>
+			<td>
+				<fieldset>
+
+					<label>
+						<input type="radio"
+						       name="<?php echo esc_attr( $this->get_field_name_in_setting( $group_key, $field_key, 'default' ) ); ?>"
+						       value="1"
+							<?php checked( 1, $this->get( 'default' ) ); ?> />
+						<span><?php echo esc_html( $this->get( 'true_label' ) ); ?></span>
+					</label>&nbsp;
+					<label>
+						<input type="radio"
+						       name="<?php echo esc_attr( $this->get_field_name_in_setting( $group_key, $field_key, 'default' ) ); ?>"
+						       value="0"
+							<?php checked( 0, $this->get( 'default' ) ); ?> />
+						<span><?php echo esc_html( $this->get( 'false_label' ) ); ?></span>
+					</label>
+
+				</fieldset>
+			</td>
+		</tr>
+		<tr>
+			<th><?php esc_html_e( 'TRUE Label', 'smart-custom-fields' ); ?></th>
+			<td>
+				<input type="text"
+				       name="<?php echo esc_attr( $this->get_field_name_in_setting( $group_key, $field_key, 'true_label' ) ); ?>"
+				       class="widefat"
+				       value="<?php echo esc_attr( $this->get( 'true_label' ) ); ?>"
+					/>
+			</td>
+		</tr>
+		<tr>
+			<th><?php esc_html_e( 'FALSE Label', 'smart-custom-fields' ); ?></th>
+			<td>
+				<input type="text"
+				       name="<?php echo esc_attr( $this->get_field_name_in_setting( $group_key, $field_key, 'false_label' ) ); ?>"
+				       class="widefat"
+				       value="<?php echo esc_attr( $this->get( 'false_label' ) ); ?>"
+					/>
+			</td>
+		</tr>
+		<tr>
+			<th><?php esc_html_e( 'Notes', 'smart-custom-fields' ); ?></th>
+			<td>
+				<input type="text"
+				       name="<?php echo esc_attr( $this->get_field_name_in_setting( $group_key, $field_key, 'notes' ) ); ?>"
+				       class="widefat"
+				       value="<?php echo esc_attr( $this->get( 'notes' ) ); ?>"
+					/>
+			</td>
+		</tr>
+	<?php
+	}
+}
