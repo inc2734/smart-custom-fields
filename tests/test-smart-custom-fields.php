9fc9205221ead440a1b5968a322b7b1a8a7ee366
Takashi Kitajima <inc@2inc.org>
2016-07-02 00:18:25 +0900

    Refactoring Smart_Custom_Fields_Test #34

--- tests/test-smart-custom-fields.php
+++ tests/test-smart-custom-fields.php
@@ -11,14 +11,16 @@ class Smart_Custom_Fields_Test extends WP_UnitTestCase {
 	 */
 	public function setUp() {
 		parent::setUp();
+
 		$this->post_ids = $this->factory->post->create_many( 5, array(
 			'post_type' => SCF_Config::NAME,
 		) );
+
 		foreach ( $this->post_ids as $post_id ) {
 			update_post_meta( $post_id, SCF_Config::PREFIX . 'repeat-multiple-data', 'dummy' );
 		}
+
 		for ( $i = 1; $i <= 5; $i ++ ) {
-			// 本来の名前とは異なるので注意
 			update_option( SCF_Config::PREFIX . $i, 'dummy' );
 		}
 
@@ -38,7 +40,7 @@ class Smart_Custom_Fields_Test extends WP_UnitTestCase {
 	/**
 	 * @group uninstall
 	 */
-	public function test_uninstall__実行したら投稿タイプSmart_Custom_Fieldsの投稿は削除() {
+	public function test_uninstall__post() {
 		Smart_Custom_Fields::uninstall();
 		$posts = get_posts( array(
 			'post_type'      => SCF_Config::NAME,
@@ -51,7 +53,7 @@ class Smart_Custom_Fields_Test extends WP_UnitTestCase {
 	/**
 	 * @group uninstall
 	 */
-	public function test_uninstall__実行したらrepeat_multiple_dataは削除() {
+	public function test_uninstall__repeat_multiple_data() {
 		Smart_Custom_Fields::uninstall();
 
 		global $wpdb;
@@ -70,7 +72,7 @@ class Smart_Custom_Fields_Test extends WP_UnitTestCase {
 	/**
 	 * @group uninstall
 	 */
-	public function test_uninstall__実行したらoptionは削除() {
+	public function test_uninstall__option() {
 		Smart_Custom_Fields::uninstall();
 
 		global $wpdb;
