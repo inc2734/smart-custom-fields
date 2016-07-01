b59033058c0e3d1a9098408f553cf0ab2b9d05c9
Takashi Kitajima <inc@2inc.org>
2016-07-01 23:55:06 +0900

    Refactoring Smart_Custom_Fields_Revision_Test #34

--- tests/test-smart-custom-fields-revision.php
+++ tests/test-smart-custom-fields-revision.php
@@ -2,6 +2,11 @@
 class Smart_Custom_Fields_Revision_Test extends WP_UnitTestCase {
 
 	/**
+	 * @var Smart_Custom_Fields_Revisions
+	 */
+	protected $Revision;
+
+	/**
 	 * @var int
 	 */
 	protected $post_id;
@@ -16,16 +21,23 @@ class Smart_Custom_Fields_Revision_Test extends WP_UnitTestCase {
 	 */
 	public function setUp() {
 		parent::setUp();
-		// カスタムフィールドを設定するための投稿
+
+		$this->Revision = new Smart_Custom_Fields_Revisions();
+
+		// The post for custom fields
 		$this->post_id = $this->factory->post->create( array(
-			'post_type' => 'post',
+			'post_type'   => 'post',
+			'post_status' => 'publish',
 		) );
-		// リビジョン用として投稿を準備
+
+		// The revision post for custom fields
 		$this->revision_id = $this->factory->post->create( array(
 			'post_type'   => 'revision',
 			'post_parent' => $this->post_id,
+			'post_status' => 'inherit',
+			'post_name'   => $this->post_id . '-autosave',
 		) );
-		// コードでカスタムフィールドを定義
+
 		add_filter( 'smart-cf-register-fields', array( $this, '_register' ), 10, 4 );
 
 		$Cache = Smart_Custom_Fields_Cache::getInstance();
@@ -45,101 +57,118 @@ class Smart_Custom_Fields_Revision_Test extends WP_UnitTestCase {
 	 * @group wp_restore_post_revision
 	 */
 	public function test_wp_restore_post_revision() {
-		// 投稿のメタデータ
+		// Meta data for the post
 		add_post_meta( $this->post_id, 'text', 'text' );
-		add_post_meta( $this->post_id, 'checkbox', 'check' );
-		add_post_meta( $this->post_id, 'text3', 'loop-text' );
+		add_post_meta( $this->post_id, 'checkbox', 1 );
 
-		// リビジョンのメタデータ
-		add_metadata( 'post', $this->revision_id, 'text', 'text-2' );
-		add_metadata( 'post', $this->revision_id, SCF_Config::PREFIX . 'repeat-multiple-data', array(
-			'checkbox3' => array( 1, 2 ),
-		) );
-		add_metadata( 'post', $this->revision_id, 'checkbox3', 'loop-check-1' );
-		add_metadata( 'post', $this->revision_id, 'checkbox3', 'loop-check-2' );
-		add_metadata( 'post', $this->revision_id, 'checkbox3', 'loop-check-3' );
+		// Meta data for the revision post
+		add_metadata( 'post', $this->revision_id, 'text', 'text-revision' );
+		add_metadata( 'post', $this->revision_id, 'checkbox', 2 );
 
-		$Revision = new Smart_Custom_Fields_Revisions();
-		$Revision->wp_restore_post_revision( $this->post_id, $this->revision_id );
+		$this->assertEquals( 'text', get_post_meta( $this->post_id, 'text', true ) );
+		$this->assertEquals( array( 1 ), get_post_meta( $this->post_id, 'checkbox' ) );
 
-		$this->assertEquals( 'text-2', SCF::get( 'text', $this->post_id ) );
-		$this->assertSame( array(), SCF::get( 'checkbox', $this->post_id ) );
-		$this->assertEquals(
-			array(
-				array(
-					'loop-check-1'
-				),
-				array(
-					'loop-check-2', 'loop-check-3',
-				),
-			), SCF::get( 'checkbox3', $this->post_id )
-		);
+		$this->Revision->wp_restore_post_revision( $this->post_id, $this->revision_id );
+
+		$this->assertEquals( 'text-revision', get_post_meta( $this->post_id, 'text', true ) );
+		$this->assertEquals( array( 2 ), get_post_meta( $this->post_id, 'checkbox' ) );
 	}
 
 	/**
 	 * @group wp_insert_post
 	 */
-	public function test_wp_insert_post_Post_IDがrevisionのときはnull() {
-		$_POST[SCF_Config::NAME] = array(
-			'text' => 'text',
+	public function test_wp_insert_post() {
+		$_REQUEST[SCF_Config::PREFIX . 'fields-nonce'] = wp_create_nonce( SCF_Config::NAME . '-fields' );
+
+		$_POST = array(
+			SCF_Config::NAME => array(
+				'text' => array( 'text' ),
+			),
 		);
-		$Revision = new Smart_Custom_Fields_Revisions();
-		$this->assertNull( $Revision->wp_insert_post( $this->post_id ) );
+		$this->Revision->wp_insert_post( $this->revision_id );
+		$this->assertEquals( 'text', SCF::get( 'text', $this->revision_id ) );
+
+		$this->Revision->wp_insert_post( $this->post_id );
+		$this->assertEquals( '', SCF::get( 'text', $this->post_id ) );
 	}
 
 	/**
-	 * フック経由でカスタムフィールドを設定
-	 *
-	 * @param array $settings 管理画面で設定された Smart_Custom_Fields_Setting の配列
-	 * @param string $type 投稿タイプ or ロール
-	 * @param int $id 投稿ID or ユーザーID
-	 * @param string $meta_type メタデータのタイプ。post or user
-	 * @return array
+	 * @group get_post_metadata
+	 */
+	public function test_get_post_metadata() {
+		update_metadata( 'post', $this->revision_id, 'text', 'text' );
+
+		$meta = $this->Revision->get_post_metadata( 'default-value', $this->post_id, 'text', true );
+		$this->assertEquals( 'default-value', $meta );
+
+		global $wp_query, $post;
+		$wp_query->is_preview = true;
+		$post = get_post( $this->post_id );
+		$meta = $this->Revision->get_post_metadata( 'default-value', $this->post_id, 'text', true );
+		$this->assertEquals( 'text', $meta );
+	}
+
+	/**
+	 * Register custom fields using filter hook
 	 */
 	public function _register( $settings, $type, $id, $meta_type ) {
-		// SCF::add_setting( 'ユニークなID', 'メタボックスのタイトル' );
-		if ( ( $type === 'post' && ( $id === $this->post_id || $id === $this->revision_id ) ) || ( $type === 'editor' ) ) {
+		if ( $type === 'post' && ( $id === $this->post_id || $id === $this->revision_id ) ) {
 			$Setting = SCF::add_setting( 'id-1', 'Register Test' );
-			// $Setting->add_group( 'ユニークなID', 繰り返し可能か, カスタムフィールドの配列 );
 			$Setting->add_group( 0, false, array(
 				array(
 					'name'  => 'text',
-					'label' => 'text field',
+					'label' => 'text',
 					'type'  => 'text',
 				),
 			) );
-			$Setting->add_group( 1, false, array(
+			$Setting->add_group( 'text-has-default', false, array(
+				array(
+					'name'    => 'text-has-default',
+					'label'   => 'text has default',
+					'type'    => 'text',
+					'default' => 'a',
+				),
+			) );
+			$Setting->add_group( 'checkbox', false, array(
 				array(
 					'name'    => 'checkbox',
 					'label'   => 'checkbox field',
 					'type'    => 'check',
-					'choices' => array( 1, 2, 3 ),
+					'choices' => array( 'a', 'b', 'c' ),
 				),
 			) );
-			$Setting->add_group( 'group-name-3', true, array(
+			$Setting->add_group( 'checkbox-has-default', false, array(
 				array(
-					'name'  => 'text3',
-					'label' => 'text field 3',
-					'type'  => 'text',
-				),
-				array(
-					'name'    => 'checkbox3',
-					'label'   => 'checkbox field 3',
+					'name'    => 'checkbox-has-default',
+					'label'   => 'checkbox has default',
 					'type'    => 'check',
-					'choices' => array( 1, 2, 3 ),
+					'choices' => array( 'a', 'b', 'c' ),
+					'default' => array( 'a' ),
 				),
 			) );
-			$settings['id-1'] = $Setting;
-
-			$Setting = SCF::add_setting( 'id-2', 'Register Test 2' );
-			$Setting->add_group( 0, false, array(
+			$Setting->add_group( 'checkbox-key-value', false, array(
 				array(
-					'name'  => 'text2-1',
-					'label' => 'text field 2-1',
+					'name'    => 'checkbox-key-value',
+					'label'   => 'checkbox key value',
+					'type'    => 'check',
+					'choices' => array( 'a' => 'apple', 'b' => 'banana', 'c' => 'carrot' ),
+					'default' => array( 'a' ),
+				),
+			) );
+			$Setting->add_group( 'group', true, array(
+				array(
+					'name'  => 'repeat-text',
+					'label' => 'repeat text',
 					'type'  => 'text',
 				),
+				array(
+					'name'    => 'repeat-checkbox',
+					'label'   => 'repeat checkbox',
+					'type'    => 'check',
+					'choices' => array( 'a', 'b', 'c' ),
+				),
 			) );
-			$settings['id-2'] = $Setting;
+			$settings[] = $Setting;
 		}
 		return $settings;
 	}
