fb59d9ed0e8b736ba0e4f35ac1d37ba8a4fe993b
Takashi Kitajima <inc@2inc.org>
2016-07-28 11:27:15 +0900

    Fix bug datepicker and colorpicker in repeatable group #41

--- js/editor-colorpicker.js
+++ js/editor-colorpicker.js
@@ -1,9 +1,9 @@
 /**
  * editor-colorpicker.js
- * Version    : 1.0.0
+ * Version    : 1.0.1
  * Author     : inc2734
  * Created    : October 21, 2014
- * Modified   :
+ * Modified   : July 28, 2016
  * License    : GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -16,8 +16,8 @@ jQuery( function( $ ) {
 		} );
 	} );
 
-	$( document ).on( 'smart-cf-after-add-group', function( e, button ) {
-		var parent = $( button ).parents( '.smart-cf-meta-box-repeat-tables' );
+	$( document ).on( 'smart-cf-after-add-group', function( e, obj ) {
+		var parent = $( obj.button ).parents( '.smart-cf-meta-box-repeat-tables' );
 		parent.find( '.smart-cf-colorpicker' ).each( function( i, e ) {
 			if ( $( e ).attr( 'disabled' ) !== 'disabled' ) {
 				$( e ).wpColorPicker();
