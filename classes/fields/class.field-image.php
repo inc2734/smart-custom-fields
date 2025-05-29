8a1abd5c8bc4f438e6ede4a0fbe719b2c5b874c2
Takashi Kitajima <inc@2inc.org>
2025-05-29 11:01:48 +0900

    Fix undefined variable warnings in file and image fields

--- classes/fields/class.field-image.php
+++ classes/fields/class.field-image.php
@@ -52,9 +52,11 @@ class Smart_Custom_Fields_Field_Image extends Smart_Custom_Fields_Field_Base {
 			esc_html__( 'Delete', 'smart-custom-fields' )
 		);
 
-		$hide_class = 'hide';
-		$image      = $btn_remove;
-		if ( $value ) {
+                $hide_class = 'hide';
+                $image      = $btn_remove;
+                $image_src  = null;
+                $image_alt  = '';
+                if ( $value ) {
 			// Usually, $value is attachment ID.
 			// If a customized, for example, $value is not an ID,
 			// Regarded the $value is file URL.
